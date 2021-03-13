f02c45eda95d18e04ec880a0efa215ae26db207c
najamavci <najamukovic@gmail.com>
2021-03-13 21:18:59 +0100

    secondmessage

--- src/GitProjectDemoClass/GitDemoClass.java
+++ src/GitProjectDemoClass/GitDemoClass.java
@@ -1,11 +1,13 @@
 package GitProjectDemoClass;
 
+import sun.jvm.hotspot.gc.parallel.PSYoungGen;
+
 public class GitDemoClass {
 
 	public static void main(String[] args) {
 
 		System.out.println("Git Demo demonstration");
-		
+		System.out.println("Naja");
 	}
 
 }
